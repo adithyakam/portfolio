5ce1bebfbba059d80704bc50754dfdc6ddbca49f
adithyakam <50355901+adithyakam@users.noreply.github.com>
2020-03-28 23:27:17 +0530

    updationg

--- js/style.js
+++ js/style.js
@@ -1,1 +1,17 @@
+// $(window).load(function() {
+//   $("#preloader").fadeOut("slow", function() {
+//     $(this).remove();
+//   });
+// });
 
+// window.load(() => {
+//   document.getElementById(preloader).classList.remove(fadeOut);
+// });
+
+document.getElementById(preloader).classList.add("fadeOut");
+
+window.addEventListener("load", showPage);
+
+function showPage() {
+  document.getElementById(preloader).classList.remove("fadeOut");
+}
