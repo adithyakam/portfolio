56d45e3a98d023dffc827af17d13e46e3f7d4273
adithyakam <50355901+adithyakam@users.noreply.github.com>
2020-05-18 11:19:25 +0530

    adding skills

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -7,60 +7,86 @@ import { ReactComponent as Svg5 } from "../../assets/nodejs.svg";
 import { ReactComponent as Svg6 } from "../../assets/npm.svg";
 import { ReactComponent as Svg7 } from "../../assets/react.svg";
 import { ReactComponent as Svg8 } from "../../assets/js.svg";
+import { ReactComponent as Svg9 } from "../../assets/expressjs.svg";
 
 const Skills = () => {
   return (
     <div className="container w-75">
-      <div className="container mt-4 center">
+      <div className="container mt-5 center w-75">
         <h1>Skills</h1>
-        <div class="row">
-          <div class="col-4 col-md-3">
+        <div class=" row row-cols-2 row-cols-sm-3 row-cols-md-3">
+          <div class=" col my-2 ">
             <div className="d-flex flex-column justify-content-center align-items-center">
               <Svg1 />
               <label>HTML</label>
             </div>
           </div>
-          <div class="col-4 col-md-3">
+          <div class=" col my-2 ">
             <div className="d-flex flex-column justify-content-center align-items-center">
               <Svg2 />
               <label>HTML</label>
             </div>
           </div>
-          <div class="col-4 col-md-3">
+          <div class=" col my-2 ">
             <div className="d-flex flex-column justify-content-center align-items-center">
               <Svg3 />
               <label>HTML</label>
             </div>
           </div>
-          <div class="col-4 col-md-3">
+          <div class=" col my-2 ">
             <div className="d-flex flex-column justify-content-center align-items-center">
               <Svg4 />
               <label>HTML</label>
             </div>
           </div>
-          <div class="col-4 col-md-3">
+          <div class=" col my-2 ">
             <div className="d-flex flex-column justify-content-center align-items-center">
               <Svg5 />
               <label>HTML</label>
             </div>
           </div>
-          <div class="col-4 col-md-3">
+          <div class=" col my-2 ">
             <div className="d-flex flex-column justify-content-center align-items-center">
               <Svg6 />
               <label>HTML</label>
             </div>
           </div>
-          <div class="col-4 col-md-3">
+          <div class=" col my-2 ">
             <div className="d-flex flex-column justify-content-center align-items-center">
               <Svg7 />
               <label>HTML</label>
             </div>
           </div>
-          <div class="col-4 col-md-3">
+          <div class=" col my-2 ">
             <div className="d-flex flex-column justify-content-center align-items-center">
               <Svg8 />
               <label>HTML</label>
             </div>
+          </div>
+          <div class=" col my-2 ">
+            <div className="d-flex flex-column justify-content-center align-items-center">
+              <Svg9 />
+              <label>ExprressJS</label>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h1>Experience</h1>
+        <div className="w-50 container">
+          <div className="my-2 text-left">
+            <h3>Program Analyst </h3>
+            <h5>Cognizant</h5>
+            <h5>2018 - present</h5>
+          </div>
+          <div className="my-2 text-nowrap" style={{ width: "8rem" }}>
+            <p>
+              Iam working as Program Analyst in Cognizant for 1 and half yrsnow.
+              <br />
+              Inhere my job is to solve the customer related issues like
+              <br />
+              optimizing,updating ,resolving.
+            </p>
           </div>
         </div>
       </div>
